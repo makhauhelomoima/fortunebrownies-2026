b3f9b479809a7ef9b868102f31eab9b0430fb410
makhauhelomoima <makhauhelomoima@gmail.com>
2026-04-27 02:39:00 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 4173
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
